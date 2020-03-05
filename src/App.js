2acf258df196f0a6fbe7523b880e3ca3039b5d16
adrianRadice <radiceadriann@gmail.com>
2020-03-04 21:30:44 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+
+  const consultarAPI = async () => {
+    const api = await fetch(
+      "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+    );
+    const frase = await api.json();
+    return frase[0];
+  };
+
+  const [frase, setFrase] = useState("");
+
+  const showFrase = async () => {
+    const frase = await consultarAPI();
+    setFrase(frase.quote);
+  };
+
+
+  useEffect(() => {
+    showFrase()
+  }, [showFrase])
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         {Object.keys(frase).length === 0 ? null : <p>{frase}</p>}
+       </header>
     </div>
   );
 }
